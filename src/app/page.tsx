af083074f42a708dc8cadee80b0dd9120b662f7f
Avinash-1-10 <avinashyede110@gmail.com>
2024-03-25 12:29:22 +0530

    hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import HeroSection from "@/components/HeroSection";
 import React from "react";
 
 const Home = () => {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center">Main contenct here</h1>
+      <HeroSection/>
     </main>
   );
 };
